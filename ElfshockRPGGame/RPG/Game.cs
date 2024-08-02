3d7a87faf32115267f31901158ab6ae8d73b5bbb
Georgi <georgi.ginduzov17d@gmail.com>
2024-08-02 22:32:00 +0300

    Add main menu screen

---
+++ ElfshockRPGGame/RPG/Game.cs
@@ -0,0 +1,45 @@
+﻿using RPG.characters;
+using System.Reflection;
+
+namespace RPG
+{
+    public class Game
+    {
+        private GameScreen _currentScreen = GameScreen.MainMenu;
+        private const int maxPointsForDistribution = 3;
+
+        public void Run()
+        {
+            while (_currentScreen != GameScreen.Exit)
+            {
+                switch (_currentScreen)
+                {
+                    case GameScreen.MainMenu:
+                        ShowMainMenu();
+                        break;
+                    case GameScreen.CharacterSelect:
+                        ShowCharacterSelect();
+                        break;
+                    case GameScreen.InGame:
+                        ShowInGame();
+                        break;
+                    case GameScreen.Exit:
+                        ExitGame();
+                        break;
+                }
+            }
+        }
+
+        private void ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Press any key to play.");
+            Console.ReadKey();
+            _currentScreen = GameScreen.CharacterSelect;
+        }
+
+        
+    }
+
+}
