91152d32056e89db8897484f3e54f5557762cc6f
Georgi <georgi.ginduzov17d@gmail.com>
2024-08-03 15:26:50 +0300

    Separate Exit and MainMenu screens

---
+++ ElfshockRPGGame/RPG/screens/ExitScreen.cs
@@ -0,0 +1,12 @@
+﻿namespace RPG.screens
+{
+    public class ExitScreen
+    {
+        public static void Run()
+        {
+            Console.Clear();
+            Console.WriteLine("Exiting the game. Goodbye!");
+            Environment.Exit(0);
+        }
+    }
+}
